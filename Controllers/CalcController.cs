c1a38eff91313cace8187d43256eb2ba6a60bb38
malli gali <lucky.galimalli@gmail.com>
2020-11-20 11:10:10 +0000

    Initial commit

---
+++ Controllers/CalcController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Calculator;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StringCalculatorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalcController : ControllerBase
+    {
+        [HttpGet]
+        public int GetSumValue(string numbersString)
+        {
+            return StringCalculator.Add(numbersString);
+        }
+    }
+}
